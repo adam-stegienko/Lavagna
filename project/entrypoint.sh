653cbbc707b7fcdfdb671f5b85c13b9219b20ce9
Adam Stegienko <107151098+adam-stegienko@users.noreply.github.com>
2022-09-08 17:10:37 +0200

    MySQL working with root

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# BASEDIR=$(dirname $0)
-
 java \
-	-Ddatasource.dialect=HSQLDB \
-	-Ddatasource.url=jdbc:hsqldb:mem:lavagna \
+	-Xms64m \
+	-Xmx128m \
+	-Ddatasource.dialect=MYSQL \
+	-Ddatasource.url=jdbc:mysql://localhost:3306/lavagna?useUnicode=true&characterEncoding=utf-8&useSSL=false \
 	-Ddatasource.username=adam \
-	-Ddatasource.password=password \
+	-Ddatasource.password=pass \
 	-Dspring.profiles.active=dev \
-	-jar /usr/src/target/lavagna-jetty-console.war
+	-jar /usr/src/target/lavagna-jetty-console.war --port 8080 --bindAddress 127.0.0.1
--- project/init.sh
+++ project/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker rm -f lavagna_dfile
-docker build -t fat_image:1.0 .
-docker run -d --name=lavagna_dfile -p 8080:8080 fat_image:1.0
+# docker rm -f lavagna_dfile
+# docker build -t fat_image:1.0 .
+# docker run -d --name=lavagna_dfile -p 8080:8080 fat_image:1.0
+docker-compose down
+docker-compose up --detach
